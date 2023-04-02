898f1d6ee856585b08befc736f54937329c001b0
DurgaCharan <durgacharan.k@flex.com>
2023-04-02 20:48:32 +0530

    added war plugin

--- src/main/Java/HelloWorldApplication.java
+++ src/main/Java/HelloWorldApplication.java
@@ -1,18 +1,20 @@
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.WebApplicationInitializer;
-import com.HelloWorld.Controllers.HelloWorld;
 
 @SpringBootApplication(scanBasePackages = {"com.HelloWorld"})
-public class HelloWorldApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class HelloWorldApplication extends SpringBootServletInitializer implements WebApplicationInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(HelloWorldApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
+
 }
