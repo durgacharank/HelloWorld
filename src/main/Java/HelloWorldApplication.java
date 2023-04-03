14c617c86bfc288a558d487aef73d556b6c6f22d
DurgaCharan <durgacharan.k@flex.com>
2023-04-03 17:39:51 +0530

    creating jar file

--- src/main/Java/HelloWorldApplication.java
+++ src/main/Java/HelloWorldApplication.java
@@ -1,17 +1,16 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.HelloWorld"})
 public class HelloWorldApplication extends SpringBootServletInitializer implements WebApplicationInitializer
 {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-    {
-        return application.sources(HelloWorldApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+//    {
+//        return application.sources(HelloWorldApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
--- src/main/Java/com/HelloWorld/Controllers/HelloWorld.java
+++ src/main/Java/com/HelloWorld/Controllers/HelloWorld.java
@@ -1,8 +1,5 @@
 package com.HelloWorld.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
